c09110b3e62f3008d484fe22dcc0ac3044cc3585
Tao Hu <taohu620@gmail.com>
2017-12-13 17:03:43 +0800

    tensorflow version

--- Stochastic_BB/example.py
+++ Stochastic_BB/example.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 
 import numpy as np
-import theano
-import theano.tensor as T
+import tensorflow as tf
 from scipy.sparse import lil_matrix
 from stochastic_bb import svrg_bb, sgd_bb
-
 
 """
     An example showing how to use svrg_bb and sgd_bb
     The problem here is the regularized logistic regression
 """
 
-__license__ = 'MIT'
-__author__ = 'Conghui Tan'
-__email__ = 'tanconghui@gmail.com'
-
 
 if __name__ == '__main__':
     # problem size
-    n, d = 1000, 100
+    #n, d = 1000, 100
+    n, d = 30, 10
 
     # randomly generate training data
     A = np.random.randn(n, d)
@@ -33,27 +28,32 @@ if __name__ == '__main__':
     # preprocess data
     tmp = lil_matrix((n, n))
     tmp.setdiag(y)
-    data = theano.shared(tmp * A)
 
-    # define objective function and gradient via Theano
+    whole_data = tmp * A
+    data = tf.placeholder(tf.float32, shape=[None,d],name="data")
+    par = tf.Variable(np.zeros((d,1)), dtype=tf.float32)
+    tild_par = tf.Variable(np.zeros((d,1)),dtype=tf.float32)
     l2 = 1e-2
-    par = T.vector()
-    loss = T.log(1 + T.exp(-T.dot(data, par))).mean() + l2 / 2 * (par ** 2).sum()
-    func = theano.function(inputs=[par], outputs=loss)
+    loss = tf.reduce_mean(tf.log(1 + tf.exp(-tf.matmul(data, par)))) + tf.reduce_sum(l2 / 2 * (par ** 2))
+    grad = tf.gradients(loss, [par])[0]
 
-    idx = T.ivector()
-    grad = theano.function(inputs=[par, idx], outputs=T.grad(loss, wrt=par),
-                           givens={data: data[idx, :]})
 
     # test SVRG-BB
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
     x0 = np.random.rand(d)
     print('Begin to run SVRG-BB:')
-    x = svrg_bb(grad, 1e-3, n, d, func=func, max_epoch=50)
+    x = svrg_bb(grad, 1e-3, n, d, tensor_x=par,func=loss, sess = sess,par = par,whole_data=whole_data, max_epoch=50)
     y_predict = np.sign(np.dot(A_test, x))
-    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict) / n))
+    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict)*1.0 / n))
+    sess.close()
+
 
     # test SGD-BB
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
     print('\nBegin to run SGD-BB:')
-    x = sgd_bb(grad, 1e-3, n, d, phi=lambda k: k, func=func, max_epoch=50)
+    x = sgd_bb(grad, 1e-3, n, d, phi=lambda k: k, tensor_x=par, func=loss, sess = sess, par = par, whole_data=whole_data, beta=0.3, max_epoch=50)
     y_predict = np.sign(np.dot(A_test, x))
-    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict) / n))
+    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict)*1.0 / n))
+    sess.close()
--- Stochastic_BB/stochastic_bb.py
+++ Stochastic_BB/stochastic_bb.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import random
-
+import tensorflow as tf
+from tqdm import tqdm
 """
     Python implementation of SVRG-BB and SGD-BB methods from the following paper:
     "Barzilai-Borwein Step Size for Stochastic Gradient Descent".
@@ -14,7 +15,11 @@ __author__ = 'Conghui Tan'
 __email__ = 'tanconghui@gmail.com'
 
 
-def svrg_bb(grad, init_step_size, n, d, max_epoch=100, m=0, x0=None, func=None,
+
+
+
+
+def svrg_bb(grad, init_step_size, n, d, sess, par, whole_data, max_epoch=100, m=0, tensor_x=None, func=None,
             verbose=True):
     """
         SVRG with Barzilai-Borwein step size for solving finite-sum problems
@@ -29,22 +34,36 @@ def svrg_bb(grad, init_step_size, n, d, max_epoch=100, m=0, x0=None, func=None,
         if verbose:
             print('Info: set m=n by default')
 
-    if x0 is None:
-        x = np.zeros(d)
-    elif isinstance(x0, np.ndarray) and x0.shape == (d, ):
-        x = x0.copy()
-    else:
-        raise ValueError('x0 must be a numpy array of size (d, )')
+
+    def get_loss(data):
+        return sess.run([func], feed_dict={"data:0": data})[0]
+
+    def get_grad(data):
+        return sess.run([grad], feed_dict={"data:0": data})[0]
+
+    def get_var(var):
+        return sess.run([var])[0]
+
+    def ass_var(var, value):
+        assign_op = var.assign(value)
+        sess.run(assign_op)
+
+
+    def get_grad_on_var(data,par):
+        ass_var(tensor_x, par)
+        return get_grad(data)
 
     step_size = init_step_size
     for k in range(max_epoch):
-        full_grad = grad(x, range(n))
-        x_tilde = x.copy()
+
+        #full_grad = grad(x, range(n))
+        full_grad = get_grad_on_var(whole_data,tensor_x)
+        x_tilde = get_var(tensor_x)
         # estimate step size by BB method
         if k > 0:
             s = x_tilde - last_x_tilde
             y = full_grad - last_full_grad
-            step_size = np.linalg.norm(s)**2 / np.dot(s, y) / m
+            step_size = np.linalg.norm(s)**2 / np.dot(np.squeeze(s), np.squeeze(y)) / m
 
         last_full_grad = full_grad
         last_x_tilde = x_tilde
@@ -52,18 +71,18 @@ def svrg_bb(grad, init_step_size, n, d, max_epoch=100, m=0, x0=None, func=None,
             output = 'Epoch.: %d, Step size: %.2e, Grad. norm: %.2e' % \
                      (k, step_size, np.linalg.norm(full_grad))
             if func is not None:
-                output += ', Func. value: %e' % func(x)
+                output += ', Func. value: %e' % get_loss(whole_data)
             print(output)
 
         for i in range(m):
             idx = (random.randrange(n), )
-            x -= step_size * (grad(x, idx) - grad(x_tilde, idx) + full_grad)
+            delta = step_size * (get_grad_on_var(whole_data[idx,:],tensor_x) - get_grad_on_var(whole_data[idx,:],x_tilde) + full_grad)
+            ass_var(tensor_x, tensor_x - delta)
+    return get_var(tensor_x)
 
-    return x
 
-
-def sgd_bb(grad, init_step_size, n, d, max_epoch=100, m=0, x0=None, beta=0, phi=lambda k: k,
-           func=None, verbose=True):
+def sgd_bb(grad, init_step_size, n, d, tensor_x, func, sess, par, whole_data, max_epoch=100, m=0, beta=0, phi=lambda k: k,
+           verbose=True):
     """
         SGD with Barzilai-Borwein step size for solving finite-sum problems
 
@@ -85,44 +104,54 @@ def sgd_bb(grad, init_step_size, n, d, max_epoch=100, m=0, x0=None, beta=0, phi=
         if verbose:
             print('Info: set beta=10/m by default')
 
-    if x0 is None:
-        x = np.zeros(d)
-    elif isinstance(x0, np.ndarray) and x0.shape == (d, ):
-        x = x0.copy()
-    else:
-        raise ValueError('x0 must be a numpy array of size (d, )')
+    def get_grad(data):
+        return sess.run([grad], feed_dict={"data:0": data})[0]
+
+    def get_loss(data):
+        return sess.run([func], feed_dict={"data:0": data})[0]
+
+    def get_var(var):
+        return sess.run([var])[0]
+
+    def ass_var(var, value):
+        assign_op = var.assign(value)
+        sess.run(assign_op)
 
     step_size = init_step_size
     c = 1
     for k in range(max_epoch):
-        x_tilde = x.copy()
+        x_tilde = get_var(tensor_x)
         # estimate step size by BB method
         if k > 1:
             s = x_tilde - last_x_tilde
             y = grad_hat - last_grad_hat
-            step_size = np.linalg.norm(s)**2 / abs(np.dot(s, y)) / m
+            step_size = np.linalg.norm(s)**2 / abs(np.dot(np.squeeze(s), np.squeeze(y))) / m
             # smoothing the step sizes
             if phi is not None:
                 c = c ** ((k-2)/(k-1)) * (step_size*phi(k)) ** (1/(k-1))
                 step_size = c / phi(k)
 
         if verbose:
-            full_grad = grad(x, range(n))
-            output = 'Epoch.: %d, Step size: %.2e, Grad. norm: %.2e' % \
-                     (k, step_size, np.linalg.norm(full_grad))
+            full_grad = get_grad(whole_data)
+            output = 'Epoch.: {}, Step size: {}, Grad. norm: {}'.format(k, step_size, np.linalg.norm(full_grad))
             if func is not None:
-                output += ', Func. value: %e' % func(x)
+                output += ', Func. value: %e' % get_loss(whole_data)
             print(output)
 
         if k > 0:
             last_grad_hat = grad_hat
             last_x_tilde = x_tilde
-        grad_hat = np.zeros(d)
+
+        if k == 0:
+            grad_hat = np.zeros((d,1))
+
+        #core logic
         for i in range(m):
             idx = (random.randrange(n), )
-            g = grad(x, idx)
-            x -= step_size * g
+            g = get_grad(whole_data[idx,:])
+            ass_var(tensor_x, tensor_x - step_size * g)
             # average the gradients
             grad_hat = beta*g + (1-beta)*grad_hat
 
-    return x
+
+    return tensor_x
